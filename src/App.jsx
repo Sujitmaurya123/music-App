3e437909f03fba1f2bca8a4ecddec15a4e233b88
Sujitmaurya123 <sujitkic6802maurya@gmail.com>
2024-07-28 16:16:39 +0530

    Create Music-App using React with music full function

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useContext } from "react"
+import Display from "./components/Display"
+import Player from "./components/Player"
+import Sidebar from "./components/Sidebar"
+import { PlayerContext } from "./context/PlayerContext"
+
+
+const App = () => {
+  const {audioRef,track,songsData}=useContext(PlayerContext)
+  return (
+    <div className="h-screen bg-black">
+      {songsData.length !==0 ?
+      <>
+      <div className="h-[90%] flex">
+          <Sidebar />
+          <Display/>
+      </div>
+      <Player />
+      </>:null
+      }
+      <audio ref={audioRef} src={track?track.file:""} preload="auto" ></audio>
+    </div>
+  )
+}
+
+export default App
